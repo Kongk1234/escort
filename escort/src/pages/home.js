dd5c92b8d8b1f96c7e039e28849691b5377450d9
Kasper <kbla.skp@edu.mercantec.dk>
2021-03-10 13:16:49 +0100

    new picture

--- escort/src/pages/home.js
+++ escort/src/pages/home.js
@@ -3,10 +3,10 @@ import {Annonce} from '../componets';
 function Home() {
     return (
         <div className="home">
-            <Annonce image="/images/Capture.png" name="Dejlige Birgitte" desc="hello kom knep mig"/>
-            <Annonce image="/images/Capture.png" name="Gay Arvad" desc="hello kom knep mig hårdt i røven"/>
-            <Annonce image="/images/Capture.png" name="Dejlige Birgitte" desc="hello kom knep mig"/>
-            <Annonce image="/images/Capture.png" name="Gay Arvad" desc="hello kom knep mig hårdt i røven"/>
+            <Annonce image="/images/meme.jpg" name="Dejlige Birgitte" desc="hello kom knep mig"/>
+            <Annonce image="/images/meme.jpg" name="Gay Arvad" desc="hello kom knep mig hårdt i røven"/>
+            <Annonce image="/images/meme.jpg" name="Dejlige Birgitte" desc="hello kom knep mig"/>
+            <Annonce image="/images/meme.jpg" name="Dadler" desc="Kom og køb, kom og køb"/>
         </div>
     );
 }
