929f1e2ac8efad71640245e076061f8da1d66d44
Kasper <kbla.skp@edu.mercantec.dk>
2021-03-10 13:11:03 +0100

    test annoncer

--- escort/src/pages/home.js
+++ escort/src/pages/home.js
@@ -3,8 +3,8 @@ import {Annonce} from '../componets';
 function Home() {
     return (
         <div className="home">
-            <h1>Hello there</h1>
-            <h2>General Kenobi</h2>
+            <Annonce image="/images/Capture.png" name="Dejlige Birgitte" desc="hello kom knep mig"/>
+            <Annonce image="/images/Capture.png" name="Gay Arvad" desc="hello kom knep mig hårdt i røven"/>
             <Annonce image="/images/Capture.png" name="Dejlige Birgitte" desc="hello kom knep mig"/>
             <Annonce image="/images/Capture.png" name="Gay Arvad" desc="hello kom knep mig hårdt i røven"/>
         </div>
