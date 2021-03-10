d0e87d2a341c1e774a91fa3061a4c3f850a71134
Kasper <kbla.skp@edu.mercantec.dk>
2021-03-10 11:51:37 +0100

    started on annoence

---
+++ escort/src/componets/annonce.js
@@ -0,0 +1,12 @@
+function annonce(image, desc){
+    return(
+        <div className="annonce">
+            <div>
+            <img src={image}></img>
+            <p>{desc}</p>
+            </div>
+        </div>
+    );
+}
+
+export default annonce;
--- escort/src/pages/home.js
+++ escort/src/pages/home.js
@@ -1,8 +1,11 @@
+import {annonce} from './annonce';
+
 function Home(){
     return(
         <div className="home">
             <h1>Hello there</h1>
             <h2>General Kenobi</h2>
+            
         </div>
     );
 }
