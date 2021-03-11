19a55055bd213f4716dab2a73997b5a26595c4bd
Kasper <kbla.skp@edu.mercantec.dk>
2021-03-11 12:56:41 +0100

    Fetch works on local json file

--- escort/src/pages/home.js
+++ escort/src/pages/home.js
@@ -1,15 +1,12 @@
-import {Annonce} from '../componets';
+import { Annonce } from '../componets';
 import '../css/annoncediv.css';
 
 function Home() {
     return (
         <div className="home">
-            <Annonce image="/images/meme.jpg" name="Dejlige Birgitte" desc="Silence wench"/>
-            <Annonce image="/images/meme.jpg" name="Ha gay" desc="I do not wish to be horny anymore"/>
-            <Annonce image="/images/meme.jpg" name="Dejlige" desc="I just want to be happy"/>
-            <Annonce image="/images/meme.jpg" name="Dadler" desc="Aaaaaaaaaaaaaaaaaaaahhhhh"/>
+        <Annonce />
         </div>
     );
 }
 
-export default Home;
+export default Home; 
