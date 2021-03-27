70b52146768ff4c2a80dc0e98ff52f3319a82164
Red wiking <theredwiking@pop-os.localdomain>
2021-03-27 11:27:42 +0100

    made connection to db

---
+++ escort-backend/mysql.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.PASSWORD,
+    port: process.env.DBPORT,
+    database: process.env.DB
+})
+
+db.connect(function(err) {
+    if (err) throw err;
+
+    console.log("Connecting to database successful");
+});
+
+module.exports = db;
--- escort-backend/server.js
+++ escort-backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 app.use(bodyParser.json())
 
